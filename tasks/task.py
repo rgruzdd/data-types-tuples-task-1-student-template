4aa3de373430ef316efdf2a8e3c7f3445ccc9310
Gruzin <gruzd.roman@inbox.ru>
2023-03-13 23:18:43 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -2,4 +2,11 @@ from typing import Tuple
 
 def get_tuple(num: int) -> Tuple[int]:
     # TODO: Add your code here
-    return tuple()
+    new_tuples = []
+    for i in str(num):
+        new_tuples.append(int(i))
+    return tuple(new_tuples)
+
+print(get_tuple(87178291199))
+
+
